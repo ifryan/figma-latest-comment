29e6d696552cbc78cb3f280102faab76c3f83dba
ryanhuang321 <ryan01@aliyun.com>
2024-01-24 00:34:48 +0800

    可自定义评论展示数量

--- code.ts
+++ code.ts
@@ -3,12 +3,18 @@ figma.showUI(__html__);
 interface comments {
   order_id: string;
 }
+
+// 展示评论数量
+var num = 5;
+
+// filekey辞典
 const tree = {
   "WujieAI-2023/11": "sVu5igAaD5FcQk0GfE3Mbi",
   "无界 AI v3.0": "sK8qmvyY5bike5hVrZThTE",
   "🧭归档文件导航": "gUPHLYpDZSf69YngFjskIP",
   "plugin test": "zPC8U2pUZMXFTdOVDiXPHM",
 }
+
 const fileName = figma.root.name as keyof typeof tree;
 console.log(tree[fileName]);
 
@@ -25,11 +31,19 @@ async function getComment(fileUrl: string) {
       // console.log(json, "\n");
       let sortedJson = json.comments.slice().sort((a: comments, b: comments) => parseInt(b.order_id) - parseInt(a.order_id));
       console.log(sortedJson);
-      const commentLink = "https://www.figma.com/file/" + tree[fileName] + "?node-id=" + sortedJson[0].client_meta.node_id + "#" + sortedJson[0].id;
-      const text = `<p style="width:100% !important;word-break:break-all;">#${sortedJson[0].order_id} ${sortedJson[0].message}</br>${commentLink}</p>`
+      const text = [];
+      for (let i = 0; i < sortedJson.length + 1; i++) {
+        // 达到约定数量或者展示全部条数
+        if (i >= num || i === sortedJson.length) {
+          figma.ui.postMessage(text);
+          // console.log(text);
+          break;
+        }
+        const commentLink = "https://www.figma.com/file/" + tree[fileName] + "?node-id=" + sortedJson[i].client_meta.node_id + "#" + sortedJson[i].id;
+        text.push(`#${sortedJson[i].order_id} ${sortedJson[i].message}</br>${commentLink}`)
+      }
       // 传递参数
-      figma.ui.postMessage(text);
-      console.log(text);
+
     } else {
       throw new Error("response.status");
     }
